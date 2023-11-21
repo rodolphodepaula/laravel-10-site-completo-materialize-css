d42a2522138bacf29fc0317905658e45999fb4af
rodolphosp <rodolphosp@gmail.com>
2023-11-21 01:00:29 -0300

    Personalizando sidenav mobile

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
     {{-- menu hamburger --}}
     <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-    <a href="{{ route('home') }}" class="brand-logo light">Nome do Site</a>
+    <a href="{{ route('home') }}" class="brand-logo light">Appcoder</a>
     {{-- menu para desktop --}}
     <ul class="right hide-on-med-and-down">
       <li><a href="{{ route('home') }}">Home</a></li>
@@ -16,17 +16,19 @@
     <ul id="slide-out" class="sidenav">
       <li><div class="user-view">
         <div class="background">
-          <img src="images/office.jpg">
+          <img src="{{ asset('images/sidenav-image.jpg') }}" alt="[imagem]" title="Appcoder">
         </div>
-        <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-        <a href="#name"><span class="white-text name">John Doe</span></a>
-        <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+        {{-- <a href="#user"><img class="circle" src="images/yuna.jpg"></a> --}}
+        <a href="#name"><span class="white-text name">Appcoder</span></a>
+        <a href="#email"><span class="white-text email">contato@appcoder.com.br</span></a>
       </div></li>
-      <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-      <li><a href="#!">Second Link</a></li>
+      {{-- <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li> --}}
+      <li><a href="#!">Menu</a></li>
       <li><div class="divider"></div></li>
-      <li><a class="subheader">Subheader</a></li>
-      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+      <li><a href="{{ route('home') }}"><i class="material-icons blue-text text-lighten-2">home</i> Home</a></li>
+      <li><a href="{{ route('servicos') }}"><i class="material-icons blue-text text-lighten-2">engineering</i>Serviços</a></li>
+      <li><a href="{{ route('galeria') }}"><i class="material-icons blue-text text-lighten-2">photo_camera</i>Galeria</a></li>
+      <li><a href="{{ route('contato') }}"><i class="material-icons blue-text text-lighten-2">contact_phone</i>Contato</a></li>
     </ul>
   </div>
 </nav>
