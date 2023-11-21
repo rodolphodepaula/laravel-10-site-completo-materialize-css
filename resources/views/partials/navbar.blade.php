e61754a50ab10fd2a125c068e55a13a354b20b09
rodolphosp <rodolphosp@gmail.com>
2023-11-21 00:35:10 -0300

    Criando sidenav

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,12 @@
-<nav class="blue"></nav>
+<nav class="blue">
+  <div class="nav-wrapper container">
+    <a href="{{ route('home') }}" class="brand-logo light">Nome do Site</a>
+    {{-- menu para desktop --}}
+    <ul class="right hide-on-med-and-down">
+      <li><a href="{{ route('home') }}">Home</a></li>
+      <li><a href="{{ route('servicos') }}">Serviços</a></li>
+      <li><a href="{{ route('galeria') }}">Galeria</a></li>
+      <li><a href="{{ route('contato') }}">Contato</a></li>
+    </ul>
+  </div>
+</nav>
